d3b55e2503c4734d014490ea221088c4cb0bd43c
Deric Cain <deric.cain@daxko.com>
2019-11-20 19:22:10 -0600

    Added content to all of the Trainer screens/connected calendar (#6)

--- src/screens/app/trainer/packages.js
+++ src/screens/app/trainer/packages.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Icon, Text } from 'react-native-elements';
+import { Icon, Text, PricingCard } from 'react-native-elements';
+import styled from 'styled-components/native';
 
 import { ContainerWithHeader } from '../../../components/container-with-header';
+import { ScrollView } from 'react-native';
+
+const PackageContainer = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+
+const packages = [
+  { name: 'The Beast', price: 100.0, duration: 60 },
+  { name: 'Hercules', price: 150.0, duration: 60 },
+  { name: 'The Hulk', price: 200.0, duration: 60 },
+];
 
 export const Packages = () => {
+  const viewPackage = () => {};
+
   return (
     <ContainerWithHeader title="Packages">
       <Text h4>This is the Packages Page</Text>
+      <PackageContainer>
+        <ScrollView>
+          {packages.map(p => (
+            <PricingCard
+              key={p.name}
+              price={`$${p.price}`}
+              color="#4f9deb"
+              title={p.name}
+              info={['1 User', 'Basic Support', 'All Core Features']}
+              button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+              onButtonPress={viewPackage}
+            />
+          ))}
+        </ScrollView>
+      </PackageContainer>
     </ContainerWithHeader>
   );
 };
--- src/screens/app/trainer/profile.js
+++ src/screens/app/trainer/profile.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Icon, Text } from 'react-native-elements';
+import { Icon, Text, Avatar, Button } from 'react-native-elements';
+import { view } from 'react-easy-state';
+import { firebase } from '@react-native-firebase/auth';
 
+import { User } from '../../../stores/user';
 import { ContainerWithHeader } from '../../../components/container-with-header';
 
-export const Profile = () => {
+export const Profile = view(({ navigation }) => {
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        navigation.navigate('Login');
+      });
+  };
+
   return (
     <ContainerWithHeader title="Profile">
-      <Text h4>This is the Profile Page</Text>
+      <Avatar rounded size="large" source={{ uri: User.photoURL }} />
+      <Text h4>{User.displayName}</Text>
+      <Button title="Sign out" type="clear" onPress={logout} />
     </ContainerWithHeader>
   );
-};
+});
 
 Profile.navigationOptions = {
   tabBarIcon: () => <Icon name="user" type="antdesign" />,
