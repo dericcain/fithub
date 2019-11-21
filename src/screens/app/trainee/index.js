0c042003ba2e3964bf71395f4e42216a8aa234b2
Maxim Selitsky <mselitsky@clubautomation.com>
2019-11-21 15:46:08 -0600

    Added profile page for trainee, just clone from trainer's profile page (#9)

--- src/screens/app/trainee/index.js
+++ src/screens/app/trainee/index.js
@@ -1,9 +1,11 @@
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import { Profile } from './profile';
 import { Trainers } from './trainers';
 
 export const Trainee = createBottomTabNavigator(
   {
+    Profile,
     Trainers,
   },
   {
