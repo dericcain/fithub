ba20911f3edd3c0f94898e1d85fd7bac90ea1f5c
Max Selitsky <mselitsky@clubautomation.com>
2019-11-21 15:24:16 -0600

    List of available trainers

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -2,12 +2,14 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import { OnBoardingRoutes } from './onboarding';
 import { Trainer } from './trainer';
+import { Trainee } from './trainee';
 
 export const AppRoutes = createAppContainer(
   createSwitchNavigator(
     {
       OnBoardingRoutes,
       Trainer,
+      Trainee,
     },
     {
       initialRouteName: 'OnBoardingRoutes',
---
+++ src/screens/app/trainee/trainers.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Icon, Text, ListItem } from 'react-native-elements';
+import styled from 'styled-components/native';
+import { firebase } from '@react-native-firebase/firestore';
+
+import { ContainerWithHeader } from '../../../components/container-with-header';
+import { ScrollView } from 'react-native';
+
+const TrainerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+
+export const Trainers = () => {
+  const [loading, setLoading] = useState(true);
+  const [trainers, setTrainers] = useState([]);
+
+  function onSnapshot(snapshot) {
+    const trainers = [];
+
+    snapshot.forEach(trainer => {
+      // TODO: use user object
+      trainers.push(trainer.data());
+    });
+
+    setTrainers(trainers);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    // TODO: move into separate repository object
+    const query = firebase
+      .firestore()
+      .collection('users')
+      .where('isTrainer', '==', true);
+
+    query.onSnapshot(onSnapshot);
+    query.get();
+  }, []);
+
+  if (loading) {
+    return <Text>Loading Trainers...</Text>;
+  }
+
+  return (
+    <ContainerWithHeader title="Trainers">
+      <Text h4>Select a Trainer</Text>
+      <TrainerContainer>
+        <ScrollView>
+          {trainers.map(trainer => (
+            <ListItem
+              title={trainer.displayName}
+              subtitle={trainer.bio}
+              leftAvatar={{ source: { uri: trainer.photoUrl } }}
+              bottomDivider
+              chevron
+            />
+          ))}
+        </ScrollView>
+      </TrainerContainer>
+    </ContainerWithHeader>
+  );
+};
+
+Trainers.navigationOptions = {
+  tabBarIcon: () => <Icon name="team" type="antdesign" />,
+};
