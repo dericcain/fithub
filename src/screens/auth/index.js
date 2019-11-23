71b80e0cf25df60bb1072213ec0681cb18d2d6fb
Deric Cain <deric.cain@daxko.com>
2019-11-23 17:44:43 -0600

    Styled choose type screen

--- src/screens/auth/index.js
+++ src/screens/auth/index.js
@@ -1,12 +1,10 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
 import { Login } from './login';
-import { Register } from './register';
 
 export const AuthRoutes = createStackNavigator(
   {
     Login,
-    Register,
   },
   {
     initialRouteName: 'Login',
