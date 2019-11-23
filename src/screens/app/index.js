b3e26be689ad361f843d6594bb57e5ecaa3c43f1
Deric Cain <deric.cain@daxko.com>
2019-11-23 07:57:59 -0600

    Finished create trainer page

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -3,9 +3,6 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { OnBoardingRoutes } from './onboarding';
 import { Trainer } from './trainer';
 import { Trainee } from './trainee';
-import { Payments } from './trainee/payments';
-import { TrainerPackages } from './trainee/trainer-packages';
-import { PaymentSuccess } from './trainee/paymentsuccess';
 
 export const AppRoutes = createAppContainer(
   createSwitchNavigator(
@@ -13,9 +10,6 @@ export const AppRoutes = createAppContainer(
       OnBoardingRoutes,
       Trainer,
       Trainee,
-      Payments,
-      TrainerPackages,
-      PaymentSuccess,
     },
     {
       initialRouteName: 'OnBoardingRoutes',
--- src/screens/app/trainee/index.js
+++ src/screens/app/trainee/index.js
@@ -2,11 +2,15 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import { Profile } from './profile';
 import { Trainers } from './trainers';
+import { Payments } from './payments';
+import { TrainerPackages } from './trainer-packages';
 
 export const Trainee = createBottomTabNavigator(
   {
     Profile,
     Trainers,
+    Payments,
+    TrainerPackages,
   },
   {
     initialRouteName: 'Trainers',
